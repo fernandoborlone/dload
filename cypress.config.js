89dd6eefba4b93a71619ee39b081698ac364c917
Fernando Borlone <fernandoborlone@gmail.com>
2023-03-18 05:46:13 -0300

    update cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve('..', 'dloadTest/cypress/config', `${file}.json`)
+  const pathToConfigFile = path.resolve('..', 'dload/cypress/config', `${file}.json`)
   return fs.readJSON(pathToConfigFile)
 }
 
